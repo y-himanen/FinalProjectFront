8ec296d9d20bd957c9f0a7649a9133ed4ac55d3b
Hanna Tuomi <hannamtuomi@gmail.com>
2020-03-25 14:02:35 +0200

    links, linklist added

---
+++ src/components/Links/LinkList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Links from "./Links";
+
+class LinkList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            linklist: []
+        }
+    }
+    componentDidMount() {
+        this.setState({loading: true})
+        const url = "http://localhost:8080/api/links"
+        fetch(url)
+            .then(response => response.json())
+            .then(response => this.setState({linklist: response}))
+    }
+
+    render() {
+        const linklist = this.state.linklist.map((item) =>
+            <Links key={item.id} product={item}/>)
+
+        function handleClick() {
+            console.log("I was clicked")
+        }
+
+        return (
+            <div>
+                {linklist}
+            </div>
+        )
+    }
+}
+
+export default LinkList;

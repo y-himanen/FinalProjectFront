b263819dcaa60e04e4e34de5b83fc0228afa1d69
MaijaYlosmaki <maija.ylosmaki@gmail.com>
2020-03-25 14:51:15 +0200

    note and notelist -components added. Lists weekly notes: working get.

--- src/pages/Week1.js
+++ src/pages/Week1.js
@@ -1,8 +1,12 @@
 import React from "react";
+import NotesList from "../components/Notes/NotesList";
 
 const Week1 = () => {
     return (
+        <div>
         <h1>Week 1</h1>
+            <NotesList />
+        </div>
     );
 }
 

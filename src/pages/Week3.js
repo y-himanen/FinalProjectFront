1d4f6a92bbf2eb5f8dfaa9f4ebce237d4e05a68c
MaijaYlosmaki <maija.ylosmaki@gmail.com>
2020-03-31 10:43:58 +0300

    week3 foundations added

---
+++ src/pages/Week3.js
@@ -0,0 +1,32 @@
+import React from "react";
+import NotesList from "../components/Notes/NotesList";
+import LinkList from "../components/Links/LinkList";
+import AddNote from "../components/Notes/AddNote";
+import Navi from "../Navi";
+
+const Week3 = () => {
+    return (
+        <div>
+            <Navi />
+            <br/>
+
+            <h1>Viikko / Week 3</h1>
+
+            <p>Ensimmäisellä viikolla opit Javan peruskäyttöä, muun muassa virtojen ja säännöllisten lausekkeiden käytön sekä poikkeuksien käsittelyn.<br />
+                Opit myös hieman hajautetusta version hallinnasta Gitin avulla.<br />
+                Täältä löydät kokoelman hyödyllisiä oppaita, "cheat sheetejä" ja helppotajuisia videoita.<br />
+                Voit lisätä omia muistiinpanojasi tai linkkejä sivun alaosaan.</p><br /><br />
+
+            <p>In week 1 you learned the basics of using Java, including streams, regular expressions, and exception handling amongst other topics.<br/>
+                You also learned a little about distributed version control using Git.<br/>
+                Here you'll find a collection of useful guides, cheat sheets and easy to understand videos.<br/>
+                You can add your own notes and/or links at the bottom of the page.</p>
+            <LinkList/>
+            <AddNote wk='3'/>
+            <NotesList wk='3'/>
+
+        </div>
+    );
+}
+
+export default Week3;
